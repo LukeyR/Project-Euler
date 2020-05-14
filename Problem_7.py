69f014126bec067d516f03e49d167da48d2a5f00
LukeyR <robertsluke3@gmail.com>
2020-05-14 19:26:53 +0100

    Finished Problem 7

--- Problem_7.py
+++ Problem_7.py
@@ -1,1 +1,11 @@
-import "Problem 3.py" as
+import Problem_3 as p3  # Using for isPrime
+
+
+counter = 1
+i = 0
+while counter <= 10001:
+    if p3.is_prime(i):
+        counter += 1
+    i += 1
+
+print(i-1)  # -1 needed as an extra one is added to i at end of loop
