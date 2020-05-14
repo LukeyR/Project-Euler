c942502a736a72b77a0416279d9cec8786d9127c
LukeyR <robertsluke3@gmail.com>
2020-05-14 19:26:38 +0100

    Re-wrote to support for using is_prime() in other files.

--- Problem_3.py
+++ Problem_3.py
@@ -14,15 +14,19 @@ import math as m
 
 # Could be made more efficient by sieve of eratosthenes, but probably not necessary in this context
 def is_prime(n):
-    for k in range(2, int(m.sqrt(n))):
+    if n <= 1:  # Needed as sqrt(4) = 2 so for loop will not enter
+        return False
+    if n == 2:  # Needed as sqrt(3) < 2 so for loop will not enter
+        return True
+    for k in range(2, int(m.sqrt(n))+1):
         if n % k == 0:
             return False
     return True
 
+if __name__ == "__main__":
+    max_num = int(input())
 
-max_num = int(input())
-
-for i in range(sqrt := int(m.sqrt(max_num))):
-    if max_num % (sqrt - i) == 0 and is_prime(sqrt - i):
-        print(sqrt - i)
-        break
+    for i in range(sqrt := int(m.sqrt(max_num))):
+        if max_num % (sqrt - i) == 0 and is_prime(sqrt - i):
+            print(sqrt - i)
+            break
